4fbc88953a0fba06922bd9c81abf3e5551848a39
Raimon Ràfols <raimon.rafols@gmail.com>
2020-07-29 14:19:10 +0100

    1010

--- demo.js
+++ demo.js
@@ -31,12 +31,12 @@ Q=1500
 f=.2
 
 // color list
-X=[
-    [W,W,N],    // terrain
-    [N,W,W],    // water
-    [W,W,W],    // snow
-    [W,S,S]     // balloon red color
-]
+// X=[
+//     [W,W,N],    // terrain
+//     [N,W,W],    // water
+//     [W,W,W],    // snow
+//     [W,S,S]     // balloon red color
+// ]
 
 // draw function (array, offset, linespan, length, time(for animation), alpha)
 dr=(q,p,m,l,t,A) => {
@@ -57,7 +57,7 @@ dr=(q,p,m,l,t,A) => {
                 // if any quad is behind the field of view enable
                 // to remove one line after all rendering has been done.
                 Z|=z <= N
-                C=X[q[k].c]
+                C=q[k].c
 
                 // quick & dirty cross product to do lighting calculations
                 e=q[k+m].y-q[k].y
@@ -68,8 +68,11 @@ dr=(q,p,m,l,t,A) => {
                 
                 // factor current color by lighting calc. approx
                 // (40% ambient + 60% lighting)
-                u=.4+.6*(u[0]*f+(t?u[1]:u[1]*f+u[2]))
-                c.fillStyle='rgba(' + C[0]*u + ',' + C[1]*u + ',' + C[2]*u + ','+A+')'
+                // u=.4+.6*(u[0]*f+(t?u[1]:u[1]*f+u[2]))
+                // C=X[q[k].c]
+                // c.fillStyle='rgba(' + C[0]*u + ',' + C[1]*u + ',' + C[2]*u + ','+A+')'
+
+                c.fillStyle='hsla(' + (!C?70:C==1?180:0) + ','+(C==2?0:7)+'0%,'+(C==2?90:50)*(.4+.6*(u[0]*f+(t?u[1]:u[1]*f+u[2])))+'%,' + A +')'
                 
                 c.beginPath()
                 c.lineTo(q[k].X,    q[k].Y)
@@ -100,21 +103,21 @@ setInterval(()=>{
         for(;(A=p.length)<N*N;B++) {
             // N random points with a central valley limited to '10'.
             // Change the '>10?10' for another value to change the depth
-            for(j=N;j--;)
+            for(j=N;j--;p.push({x: k*N + W*sin(B*f), y:(random()/2+.3*(n>10?10:n))*1050-Q, z: F+B * N, c:0}))
                 k=N/2-j,
-                n=N/abs(k),
-                p.push({x: k*N + W*sin(B*f), y:(random()/2+.3*(n>10?10:n))*1050-Q, z: F+B * N, c:0})
+                n=N/abs(k)
+                
             
 
             // smooth the points with it's 3 close neighbours.
             // this proces is done 3 times, otherwise results are too rough
-            for(Z=3;B&&--Z;)
-                for(j=N;j--;)
+            for(Z=3;B>2&&--Z;)
+                for(j=N;j--;p[k].c=p[k].y<W?2:p[k].y<Q?0:1)
                     k=A-N-j,
-                    p[k].y=(p[k].y + p[k-1].y + p[k-N].y)/3,
+                    p[k].y=(p[k].y + p[k-1].y + p[k-N].y)/3
 
                     // assign color index based on height
-                    p[k].c=p[k].y<W?2:p[k].y<Q?0:1
+                    //-- moved to for statement
         }
 
         // draw slices terrain from farthest to nearest to do a 'natural'
@@ -125,12 +128,13 @@ setInterval(()=>{
 
         // if any layer went behind the screen, remove one line of points
         // a new one will be randomly generated on next iteration at the back
+        // terser will move this statement to the for loop below
         Z&&(p=p.slice(N))
 
         // draw the balloon, adding 2 iterations vertically to draw the basket
         // or something similar to a basket ;)
         for(;++i<=S;)
-            for(n=S;n--;) {
+            for(n=S;n--;dr([{x:s*g,  y:i*T,   z:s*e+V, c:x}, {x:s*m,  y:i*T,   z:s*d+V, c:x}, {x:b*g,  y:i*T+T, z:b*e+V, c:x}, {x:b*m,  y:i*T+T, z:b*d+V, c:x}],0,2,4,0,1)) {
                 //color would be alternating white/red each vertical stripe
                 x=2+n%2
 
@@ -142,10 +146,7 @@ setInterval(()=>{
                 b=W*sin(i*f+f)
 
                 //generate the points and draw
-                dr([{x:s*g,  y:i*T,   z:s*e+V, c:x},
-                    {x:s*m,  y:i*T,   z:s*d+V, c:x},
-                    {x:b*g,  y:i*T+T, z:b*e+V, c:x},
-                    {x:b*m,  y:i*T+T, z:b*d+V, c:x}],0,2,4,0,1)
+                // -- moved to for loop statement
             }
     }
 })
